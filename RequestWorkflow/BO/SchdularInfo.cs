c1ed35a4403d9bfc32fe605c35f84bcf412b516a
Amol Rogye <amolrogye33@gmail.com>
2020-11-12 16:36:59 +0530

    Save

---
+++ RequestWorkflow/BO/SchdularInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RequestWorkflow.BO
+{
+    public class SchdularInfo
+    {
+        public int schedule_id { get; set; }
+        public string name { get; set; }
+        public int enabled { get; set; }
+        public int freq_type { get; set; }
+        public int freq_interval { get; set; }
+        public int freq_subday_type { get; set; }
+        public int freq_subday_interval { get; set; }
+        public int freq_relative_interval { get; set; }
+        public int freq_recurrence_factor { get; set; }
+        public int active_start_date { get; set; }
+        public int active_end_date { get; set; }
+        public int active_start_time { get; set; }
+        public int active_end_time { get; set; }
+    }
+}
--- RequestWorkflow/Controllers/RequestController.cs
+++ RequestWorkflow/Controllers/RequestController.cs
@@ -1,7 +1,10 @@
-﻿using RequestWorkflow.Models;
+﻿using RequestWorkflow.BO;
+using RequestWorkflow.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +13,7 @@ namespace RequestWorkflow.Controllers
     public class RequestController : Controller
     {
         DBEntities db = null;
+        string strConString = @"Data Source=.;Initial Catalog=msdb;Integrated Security=True";
         public RequestController()
         {
             db = new DBEntities();
@@ -140,5 +144,161 @@ namespace RequestWorkflow.Controllers
                 }, JsonRequestBehavior.AllowGet);
             }
         }
+
+        public ActionResult UpdateSchedule()
+        {
+            FillDropDown();
+            return View();
+        }
+
+        public ActionResult UpdateSchedular(SchdularInfo schdularInfo)
+        {
+            try
+            {
+
+                StringBuilder sb = new StringBuilder();
+
+                //sb.Append(" USE msdb GO ");
+
+                sb.Append(" EXEC dbo.sp_update_schedule ");
+                sb.Append("@schedule_id = 11  ");
+                sb.Append($", @enabled =  {schdularInfo.enabled} ");
+                sb.Append($", @freq_type = {schdularInfo.freq_type} ");
+                sb.Append($", @freq_interval = {schdularInfo.freq_interval}");
+                sb.Append($", @freq_subday_type = {schdularInfo.freq_subday_type}");
+                sb.Append($", @freq_subday_interval = {schdularInfo.freq_subday_interval}");
+                sb.Append($", @freq_relative_interval =  {schdularInfo.freq_relative_interval} ");
+                sb.Append($", @freq_recurrence_factor =  {schdularInfo.freq_recurrence_factor} ");
+                sb.Append($", @active_start_date =  {schdularInfo.active_start_date}  ");
+                sb.Append($", @active_end_date =   {schdularInfo.active_end_date} ");
+                sb.Append($", @active_start_time =  {schdularInfo.active_start_time} ");
+                sb.Append($", @active_end_time =  {schdularInfo.active_end_time} ");
+
+                
+                using (SqlConnection con = new SqlConnection(strConString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sb.ToString(), con);
+                    cmd.ExecuteNonQuery();
+                }
+
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult GetSchedularInfo(int id)
+        {
+            SchdularInfo info = null;
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(" SELECT schedule_id, name, enabled,freq_type, freq_interval,freq_subday_type, ");
+                sb.Append(" freq_subday_interval, freq_relative_interval, freq_recurrence_factor, active_start_date, ");
+                sb.Append(" active_end_date, active_start_time, active_end_time ");
+                sb.Append($" FROM sysschedules WHERE schedule_id = {id} ");
+
+                using (SqlConnection con = new SqlConnection(strConString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(sb.ToString(), con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if(dr != null)
+                    {
+                        dr.Read();
+                        info = new SchdularInfo();
+                        info.schedule_id = Convert.ToInt32(dr["schedule_id"]);
+                        info.name = Convert.ToString(dr["name"]);
+                        info.enabled = Convert.ToInt32(dr["enabled"]);
+                        info.freq_type = Convert.ToInt32(dr["freq_type"]);
+                        info.freq_interval = Convert.ToInt32(dr["freq_interval"]);
+                        info.freq_subday_type = Convert.ToInt32(dr["freq_subday_type"]);
+                        info.freq_subday_interval = Convert.ToInt32(dr["freq_subday_interval"]);
+                        info.freq_relative_interval = Convert.ToInt32(dr["freq_relative_interval"]);
+                        info.freq_recurrence_factor = Convert.ToInt32(dr["freq_recurrence_factor"]);
+                        info.active_start_date = Convert.ToInt32(dr["active_start_date"]);
+                        info.active_end_date = Convert.ToInt32(dr["active_end_date"]);
+                        info.active_start_time = Convert.ToInt32(dr["active_start_time"]);
+                        info.active_end_time = Convert.ToInt32(dr["active_end_time"]);
+                    }
+                    dr.Close();
+                }
+
+                return Json(new { isSuccess = true, info }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json(new { isSuccess = false, info }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        private void FillDropDown()
+        {
+            ViewBag.ScheduleType = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = "Once", Value = "1"},
+                new SelectListItem(){Text = "Daily", Value = "4"},
+                new SelectListItem(){Text = "Weekly", Value = "8"},
+                new SelectListItem(){Text = "Monthly", Value = "16"}
+            };
+
+            List<SelectListItem> lstWeeklyInterval = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = "Sunday", Value = "1"},
+                new SelectListItem(){Text = "Monday", Value = "2"},
+                new SelectListItem(){Text = "Tuesday", Value = "4"},
+                new SelectListItem(){Text = "Wednesday", Value = "8"},
+                new SelectListItem(){Text = "Thursday", Value = "16"},
+                new SelectListItem(){Text = "Friday", Value = "32"},
+                new SelectListItem(){Text = "Saturday", Value = "64"},
+            };
+
+            List<SelectListItem> lstMonthlyRelative = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = "Sunday", Value = "1"},
+                new SelectListItem(){Text = "Monday", Value = "2"},
+                new SelectListItem(){Text = "Tuesday", Value = "3"},
+                new SelectListItem(){Text = "Wednesday", Value = "4"},
+                new SelectListItem(){Text = "Thursday", Value = "5"},
+                new SelectListItem(){Text = "Friday", Value = "6"},
+                new SelectListItem(){Text = "Saturday", Value = "7"},
+                new SelectListItem(){Text = "Day", Value = "8"},
+                new SelectListItem(){Text = "Weekday", Value = "9"},
+                new SelectListItem(){Text = "Weekend day", Value = "10"},
+            };
+
+            ViewBag.DailyFrequencyInterval = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text = "Hour(s)", Value="8"},
+                new SelectListItem(){Text = "Minute(s)", Value="4"},
+                new SelectListItem(){Text = "Second(s)", Value="2"}
+            };
+
+
+            List<SelectListItem> lstSchedular = new List<SelectListItem>();
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT schedule_id, name FROM sysschedules WHERE name IN ('LeaveScheduler','HelpDeskScheduler','TestSchedule')", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if(dr != null)
+                {
+                    while (dr.Read())
+                    {
+                        lstSchedular.Add(new SelectListItem() { Text = Convert.ToString(dr["name"]), Value = Convert.ToString(dr["schedule_id"]) });
+                    }
+                }
+
+                dr.Close();
+            }
+
+            ViewBag.SchedularList = lstSchedular;
+        }
+
     }
 }
